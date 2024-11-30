c397758b5b55e7c9d90246aab3b955ebe119cd7c
supriya <angalwarsupriya@gmail.com>
2024-12-01 00:09:48 +0530

    removed activelink status

--- src/components/MobileNav/index.jsx
+++ src/components/MobileNav/index.jsx
@@ -1,37 +1,26 @@
 import './index.css'
 
-const status = {
-    home:'HOME',
-    skills:"SKILLS",
-    projects:'PROJECTS',
-    contact:'CONTACT'
-}
-
 function MobileNav(props) { 
 
-  const {menuStatus, changeActiveLinkStatus, activeLinkStatus} = props
-  
+  const {menuStatus} = props
 
   const menuStaAct = menuStatus ? '' : 'active'
 
-  const change = ()=>{
-    console.log('supiya')
-  }
-  
+
   return (
     <div className={`mobile-nav-con ${menuStaAct}`}>
         <ul className='nav-bar-in-sm'>
             <li className='list-item-sm' >
-                <a href='#hero-section' className='menu-item-sm' style={{color: activeLinkStatus===status.home ? 'skyblue' : 'white', fontSize:activeLinkStatus===status.home ? '1.2rem' : '1.05rem'}} onClick={() => changeActiveLinkStatus(status.home)}>Home</a>
+                <a href='#hero-section' className='menu-item-sm menu-item' >Home</a>
             </li>
             <li className='list-item-sm' >
-                <a href='#skills-section' className='menu-item-sm' style={{color: activeLinkStatus===status.skills ? 'skyblue' : 'white', fontSize:activeLinkStatus===status.skills ? '1.2rem' : '1.05rem'}} onClick={() => changeActiveLinkStatus(status.skills)}>Skills</a>
+                <a href='#skills-section' className='menu-item-sm menu-item'  >Skills</a>
             </li>
             <li className='list-item-sm'>
-                <a href='#my-projects' className='menu-item-sm' style={{color: activeLinkStatus===status.projects ? 'skyblue' : 'white', fontSize:activeLinkStatus===status.projects ? '1.2rem' : '1.05rem'}} onClick={() => changeActiveLinkStatus(status.projects)}>Projects</a>
+                <a href='#my-projects' className='menu-item-sm menu-item' >Projects</a>
             </li>
             <li className='list-item-sm'>
-                <a href='#contact-section' className='menu-item-sm' style={{color: activeLinkStatus===status.contact ? 'skyblue' : 'white', fontSize:activeLinkStatus===status.contact ? '1.2rem' : '1.05rem'}} onClick={() => changeActiveLinkStatus(status.contact)}>Contact Me</a>
+                <a href='#contact-section' className='menu-item-sm menu-item' >Contact Me</a>
             </li>
         </ul>
     </div>
@@ -39,5 +28,3 @@ function MobileNav(props) {
 }
 
 export default MobileNav
-
-
