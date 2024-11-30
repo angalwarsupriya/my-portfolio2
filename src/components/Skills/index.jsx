681d4151c47d12104cbab22aabd0e43e3641bd1a
supriya <angalwarsupriya@gmail.com>
2024-11-30 17:04:48 +0530

    Added Web.config to ensure correct MIME types for JavaScript files

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,9 +1,52 @@
 import './index.css'
 
 import SkillCard from '../SkillCard'
-import { skillsData } from '../../utilities/skillsData'
+
 
 import { useState } from 'react'
+
+const skillsData = [
+  {
+  title:'Frontend',
+  icon:"images/responsive.png",
+  skillsOb:[
+      {skill:"HTML", percentage:"80%"},
+      {skill:"CSS", percentage:"90%"},
+      {skill:"Javascript", percentage:"85%"},
+      {skill:"React JS", percentage:"95%"},
+      {skill:"Boostarp", percentage:"75%"},
+  ]
+},     
+{
+  title:'Backend',
+  icon:"images/digital-transformation_15594457.png",
+  skillsOb:[
+      {skill:"Node JS", percentage:"70%"},
+      {skill:"Express JS", percentage:"65%"},
+  ]
+},
+{
+  title:'Tools',
+  icon:"images/work_16768526.png",
+  skillsOb:[
+      {skill:"Git & Github", percentage:"70%"},
+      {skill:"VS code", percentage:"75%"},
+      {skill:"Webpack", percentage:"60%"},
+      {skill:"Responsive Design", percentage:"70%"},
+  ]
+},
+{
+  title:'Soft Skills',
+  icon:"images/emerging-industries_15568329.png",
+  skillsOb:[
+      {skill:"Problem Solving", percentage:"80%"},
+      {skill:"Colabration", percentage:"85%"},
+      {skill:"Atention to Detail", percentage:"78%"},
+  ]
+}
+
+]
+
 
 function Skills() {
 
